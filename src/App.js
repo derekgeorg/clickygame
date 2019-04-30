f031e81a8fe3965478009460320ce1de0a036478
Derek <45375006+derekgeorg@users.noreply.github.com>
2019-04-29 23:32:53 -0500

    styling for app.js completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-dark bg-primary">
+        <ul>
+          <li>
+            Clicky Game
+          </li>
+          <li class>
+            You guessed <span id="guessValue">Value</span>
+          </li>
+          <li>
+            Score: <span id="yourScore">0</span> | Top Score: <span id="topScore">0</span>
+          </li>
+        </ul>
+      </nav>
+      <div className="jumbotron jumbotron-fluid">
+        <h1 className="display-4">Clicky Game!</h1>
+        <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
+      </div>
+      <main className="container">
+      <div className="imgDiv"><img class="img" src="/assets/img/react.png" alt="react js logo"></img></div>
+      <div className="imgDiv"><img class="img" src="/assets/img/atom.png" alt="atom logo"></img></div>
+      <div className="imgDiv"><img class="img" src="/assets/img/mongo.png" alt="monog logo"></img></div>
+      <div className="imgDiv"><img class="img" src="/assets/img/nodejs-512.png" alt="nodejs logo"></img></div>
+      {/* <div className="imgDiv"><img class="img" src="/assets/img/sketch.png"></img></div> */}
+      </main>
+      <footer className="footer">
+        <div className="bottom">
+        Clicky Game! <img class="footerImg" alt="react" src="assets/img/react.png"></img>
+                </div>
+      </footer>
     </div>
   );
 }
